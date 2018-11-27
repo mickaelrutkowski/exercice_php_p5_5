cf04112c7af267b2287b015ce63c811070823811
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-27 15:53:44 +0100

    exercice_php_p5E5

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+$dep = array( 80 =>'Somme', 60=>'Oise', 62 =>'Pas de Calais', 02 =>'Aisne', 59 =>'Nord',);
+echo 'Vous vous trouvez dans le départempent' . ' ' .$dep[80] . " HAUT DE FRANCE.";
+?>
